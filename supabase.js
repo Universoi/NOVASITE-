504f3ff4e99f32ce347cd94d133eae9a67063271
Universoi <bbmhg03@gmail.com>
2026-01-18 20:25:35 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
 // Substitua com a URL e a chave anônima do seu projeto Supabase
-const supabaseUrl = 'https://qqspmuaukoaetilbaflv.supabase.co';
-const supabaseKey = 'sb_publishable_DN831m2SDg4cVvfn8Vc0pA_VZGfs_9u';
+const supabaseUrl = 'https://zkhradhpwakvtgsilsyj.supabase.co';
+const supabaseKey = 'sb_publishable_UrUxPsPD3XBTANwwp3tZOA_-REppnf3';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
